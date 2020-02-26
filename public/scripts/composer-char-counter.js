ee66a4178ed09efeaca64805b87e604cd0652744
Shuchita <shuchita@live.com>
2020-02-26 02:48:27 +0000

    Display old tweets

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+  const txt = $('textarea');
+  let $charsLeft;
+
+  txt.on("keyup", function () {
+    $charsLeft = (140 - $(this).val().length);
+    $counter = $(this).siblings().find(".counter");
+    $counter.text($charsLeft);
+
+    if ($charsLeft < 0) {
+      $counter.addClass('fontred');
+    } else {
+      $counter.removeClass('fontred');
+    }
+
+  });
+});
+
+// if ($charsLeft < 0) {
+//   $counter.addClass("fontRed");
+// } else {
+//   $counter.removeClass("fontRed");
+// }
