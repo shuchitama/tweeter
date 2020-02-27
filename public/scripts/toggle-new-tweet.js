a846bf08b079273a4eae1d25c6750bf4e7459c60
Shuchita <shuchita@live.com>
2020-02-27 04:31:54 +0000

    code review, multiple small changes

--- public/scripts/toggle-new-tweet.js
+++ public/scripts/toggle-new-tweet.js
@@ -1,6 +1,9 @@
-$(document).ready(function () {
-
-  $(".toggleBtn").click(() => {
+const toggleNewTweet = function () {
+  $(".toggle-btn").click(() => {
     $(".new-tweet").slideToggle(400);
   })
+}
+
+$(document).ready(function () {
+  toggleNewTweet();
 })
